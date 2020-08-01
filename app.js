00d2671a502bd9d217c0c4b1fdfeb034e287418a
boris <brmiranda009@gmail.com>
2020-08-01 16:20:18 -0600

    ya configurado con jquery

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("jquery");
 console.log("Hola mundo");
+$(document).ready(function () {
+    console.log("pagina guardada...");
+    $("h1").text("SOBRE ESCRITURA");
+});
--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
-
+import "jquery";
 console.log("Hola mundo");
+
+$(document).ready(function () {
+  console.log("pagina guardada...");
+
+  $("h1").text("SOBRE ESCRITURA");
+});
