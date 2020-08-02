b054bee8ae064eaad2a5584c681a2450c949e66f
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-08-02 16:56:26 -0600

    ya hay comunicacion con el jquery en home

--- public/js/auth/authController.js
+++ public/js/auth/authController.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jquery");
 console.log("dentro de auth controller");
-$(function () {
-  // Evento boton inicio sesion
-  $("#btnInicioSesion").click(function () {
-    console.log("dentro del onclick");
-    $("#emailSesion").val("");
-    $("#passwordSesion").val("");
-    $("#modalSesion").modal("open");
-  });
+$(() => {
+    // Evento boton inicio sesion
+    $("#btnInicioSesion").click(() => {
+        console.log("dentro del onclick");
+        $("#emailSesion").val("");
+        $("#passwordSesion").val("");
+        //$("#modalSesion").modal("open");
+    });
 });
