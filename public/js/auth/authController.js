97840be48a5d63e89eeef4fc0980592b3ee2a5e6
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-08-02 18:34:47 -0600

    ARREGLANDO EL MALDITO MODAL

--- public/js/auth/authController.js
+++ public/js/auth/authController.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jquery");
+require("materialize");
 console.log("dentro de auth controller");
-$(() => {
+$(document).ready(function () {
+    $(".modal").modal();
     // Evento boton inicio sesion
     $("#btnInicioSesion").click(() => {
         console.log("dentro del onclick");
         $("#emailSesion").val("");
         $("#passwordSesion").val("");
-        //$("#modalSesion").modal("open");
+        $("#modalSesion").modal("open");
     });
 });
--- public/js/auth/authController.ts
+++ public/js/auth/authController.ts
@@ -1,12 +1,16 @@
 import "jquery";
-import "materialize-css";
+import "materialize";
 console.log("dentro de auth controller");
-$(() => {
+
+$(document).ready(function () {
+  //ESTA LINEA EES NECESARIA PARA INICIALIZAR EL PUTO MODAL
+  $(".modal").modal();
+
   // Evento boton inicio sesion
   $("#btnInicioSesion").click(() => {
     console.log("dentro del onclick");
     $("#emailSesion").val("");
     $("#passwordSesion").val("");
-    $("#myModal").modal("open");
+    $("#modalSesion").modal("open");
   });
 });
